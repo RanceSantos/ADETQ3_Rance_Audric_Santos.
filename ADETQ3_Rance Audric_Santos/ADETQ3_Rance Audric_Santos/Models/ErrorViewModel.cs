43280b5bcc335dbb7b8ff1739febdba28c6ef9e2
RANCE AUDRIC SANTOS <1247-18@itmlyceumalabang.onmicrosoft.com>
2020-10-23 22:36:15 +0800

    Commit a message

---
+++ ADETQ3_Rance Audric_Santos/ADETQ3_Rance Audric_Santos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ADETQ3_Rance_Audric_Santos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
